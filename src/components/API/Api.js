d50d24b2544fa7bdff818105df340f160b6b0c80
kubdev36 <kub336655@gmail.com>
2026-08-19 16:42:15 +0700

    [new] cloning API file

--- src/components/API/Api.js
+++ src/components/API/Api.js
@@ -1,12 +1,102 @@
-import axios from "axios";
+import axios from "axios"
 
-export async function callApi(method, url, data, headers) {
-  const response = await axios({
-    method,
-    url,
-    data,
-    headers,
+export const callApi = (method, url, data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (method === 'get') {
+        let res = await axios.get(url, {
+          headers: {
+            token: JSON.parse(localStorage.getItem('token')) || JSON.parse(sessionStorage.getItem('token'))
+          },
+          withCredentials: true
+        })
+        // console.log(method,url,res.data)
+        resolve(res.data)
+      } else {
+        let res = await axios.post(url, data, {
+          headers: {
+            token: JSON.parse(localStorage.getItem('token')) || JSON.parse(sessionStorage.getItem('token')),
+          },
+          withCredentials: true
+        })
+        // console.log(method,url,res.data)
+        resolve(res.data)
+
+
+              // const response = await axios({
+              //     url: url,
+              //     method: "post",
+              //     headers: {
+              //         "Content-Type": "application/x-www-form-urlencoded",
+              //         'token': JSON.parse(localStorage.getItem('token')) || JSON.parse(sessionStorage.getItem('token'))
+              //     },
+              //     data: data
+              // });
+              // console.log(response.data)
+              // resolve(response.data)
+  
+
+  
+      
+
+
+      }
+    } catch (e) {
+      console.error("API ERROR:", e.response?.data || e);
+
+      reject(e); // QUAN TRỌNG
+    }
   });
+}
 
-  return response.data;
+export const Download = (url, data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+
+      await axios.post(url, data,
+        { responseType: 'blob' },
+        {
+          headers: {
+            token: JSON.parse(localStorage.getItem('token')) || JSON.parse(sessionStorage.getItem('token'))
+          },
+
+          withCredentials: true
+        }).then(
+          function (res) {
+            resolve(res.data)
+          }
+        )
+
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+
+export const From = (url, data, name) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const formData = new FormData()
+      formData.append('image',data)
+      formData.append('name',name)
+      await axios.post(url, formData,
+    
+        {
+          headers: {
+            token: JSON.parse(localStorage.getItem('token')) || JSON.parse(sessionStorage.getItem('token')),
+            'content-type': 'multipart/form-data'
+          },
+
+          withCredentials: true
+        }).then(
+          function (res) {
+            resolve(res.data)
+          }
+        )
+
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
